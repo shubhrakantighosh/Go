18b971adb9f50c49549cc3b1f6315030df1db6db
shubhrakantighosh <shubhrakantighosh1996@gmail.com>
2023-03-15 23:48:23 +0530

    update

--- array/array_declaration.go
+++ array/array_declaration.go
@@ -10,6 +10,12 @@ func main() {
 	var arr1 = [2]int{1, 3}
 	arr2 := [3]int{3, 1, 3}
 
-	fmt.Println(arr1, arr2)
+	var array [3]int
+
+	array[0] = 1
+	array[1] = 1
+	array[2] = 3
+
+	fmt.Println(arr1, arr2, array)
 
 }
